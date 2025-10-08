ec5a64fe052c7e42366109b9d13433be409ca245
ManjakaMC <manjakaandrianavalona12@gmail.com>
2025-10-08 11:40:01 +0300

    dashbord maj

---
+++ view/dashbord.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tableau de bord</title>
+    <link rel="stylesheet" href="./assets/dashbord.css">
+</head>
+<body>
+    <header>
+           <div class="navbar">
+          <div class="logo1">
+                 <a href="">Cash track</a>
+          </div>
+            <ul class="links">
+                    <li><a href="">Tableau de bord</a></li>
+                    <li><a href="">Total des credits</a></li>
+                    <li><a href="">Total des debits</a></li>
+                    <li><a href="">A propos</a></li>
+                    <li><a href="">Contact</a></li>
+            </ul>
+            </div>
+    </header>
+     
+</body>
+</html>
--- view/index.php
+++ view/index.php
@@ -3,15 +3,17 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Login</title>
 </head>
 <body>
     <h1>azerty</h1>
     <h2>ma modification</h2>
-    <a href="insert_transaction.php"> inserer</a>
-    <form action="">
-        <label for=""></label>
-        <input type="text">
-    </form>
+        <div class="dddddd">
+            <a href="insert_transaction.php"> inserer</a>
+            <a href="dashbord.php">se connecter</a>
+        </div>
+    
+
+   
 </body>
 </html>
