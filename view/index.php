e04b70f5cb2927c683f3199d2f652f216693e363
ManjakaMC <manjakaandrianavalona12@gmail.com>
2025-10-09 16:06:02 +0300

    mmc

--- view/index.php
+++ view/index.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
+    <link rel="stylesheet" href="./assets/index.css">
 </head>
 <body>
    
@@ -12,8 +13,26 @@
             <a href="dashbord.php">se connecter</a>
         </div>
     
+    <form action="../controller/UsersController.php" method="POST"  class="insertform">
+     
+      <section>
+        <div class="premiere">
+            <p> <label for="nom"> Nom</label><br>
+               <input type="text" id="nom" name="nom" placeholder="Nom"></p>
+            <p> <label for="email"> Email</label>
+                <input type="text" id="email" name="email" placeholder="Votre e-mail"><br></p>
+            <p> <label for="montant">Mot de passe</label>
+                <input type="text" id="mdp" name="mdp" placeholder="Mot de passe"><br></p>
+            <p> <label for="description">Mot de passe</label>
+               <input type="text" id="mdp" name="mdp" placeholder="Confirmer mot de passe"></p>
+          
+        </div>
+    
+             <div class="deuxieme">
+            <button>Submit</button>
+        </div>
 
    
-    <h1>Manjaka Andrianavalona Albert Etienne Mamisoa Elisa Collette</h1>
+    
 </body>
 </html>
